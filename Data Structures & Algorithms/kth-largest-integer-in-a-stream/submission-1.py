84f32a99d9c75f1ee85d2aff76683af0bf41a827
Yixuan Liang <100000217+LIANGYIXUAN3335@users.noreply.github.com>
2026-04-30 14:52:48 -0700

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.py
@@ -0,0 +1,16 @@
+import heapq
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        heapq.heapify(nums)
+        self.minHeap = nums
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minHeap,val)
+        while len(self.minHeap) >= self.k:
+            res = heapq.heappop(self.minHeap)
+        heapq.heappush(self.minHeap,res)
+        return res
