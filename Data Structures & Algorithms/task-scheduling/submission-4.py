6d563664b11af2d85c0ac389b80e4d0743deee67
Yixuan Liang <100000217+LIANGYIXUAN3335@users.noreply.github.com>
2026-04-30 19:07:50 -0700

    Add: task-scheduling - submission-4

---
+++ Data Structures & Algorithms/task-scheduling/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        taskMap = [0] * 26
+        for task in tasks:
+            taskMap[ord(task) - ord('A')] += 1
+        taskCount = 0
+        res = 0
+        while taskCount < len(tasks):
+            curLen = 0
+            taskMap.sort(reverse = True)
+            for i in range(26):
+                if curLen > n:
+                    break
+                if taskMap[i] > 0:
+                    taskMap[i] -= 1
+                    res +=  1
+                    curLen += 1
+                    taskCount += 1
+            if taskCount < len(tasks) and n - curLen + 1 > 0:
+                res += n - curLen + 1
+        return res  
