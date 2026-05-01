70b6a1542ba150a2d2597907caa4db5876e2c9e9
Yixuan Liang <100000217+LIANGYIXUAN3335@users.noreply.github.com>
2026-05-01 16:57:23 -0700

    Add: design-twitter-feed - submission-3

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-3.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+class Twitter:
+
+    def __init__(self):
+        self.followMap = defaultdict(set)
+        self.postMap = defaultdict(list)
+        self.curTimeStamp = 1
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.postMap[userId].append((self.curTimeStamp, tweetId))
+        self.curTimeStamp += 1
+        
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        res = []
+        for i in self.followMap[userId]:
+            res.extend(self.postMap[i])
+        res.extend(self.postMap[userId])
+        res.sort(key = lambda x : x[0])
+        res = [x[1] for x in res[-10 :]]
+        res.reverse()
+        return res
+        
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId == followeeId:
+            return 
+        self.followMap[followerId].add(followeeId)
+        
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.followMap[followerId].discard(followeeId)
+        
