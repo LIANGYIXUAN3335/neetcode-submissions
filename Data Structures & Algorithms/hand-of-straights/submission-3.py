6b137d6b19ced041bfe43785ac6cc8564c2c4b1f
Yixuan Liang <100000217+LIANGYIXUAN3335@users.noreply.github.com>
2026-05-13 18:40:57 -0700

    Add: hand-of-straights - submission-3

---
+++ Data Structures & Algorithms/hand-of-straights/submission-3.py
@@ -0,0 +1,21 @@
+from sortedcontainers import SortedDict
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+        countMap = SortedDict()
+        for i in hand:
+            countMap[i] = countMap.get(i, 0) + 1
+        while countMap:
+            cur = start = countMap.keys()[0]
+            for _ in range(groupSize):
+                if cur not in countMap:
+                    return False
+                else:
+                    countMap[cur] -= 1
+                    if countMap[cur] == 0:
+                        del countMap[cur]
+                cur += 1
+        return True
+        
